6177f343b673a59e719bf607f429345f8c419498
Samantha CK Leung <samanthackleung@gmail.com>
2019-06-26 20:44:15 -0400

    Lesson 4

--- src/board.js
+++ src/board.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './board.scss';
+import Snake from './snake';
 
-const boardSize = 720
+const boardSize = 720;
 const cellSize = boardSize / 30;
 
 class Board extends React.Component {
 
-    // constructor here
     constructor(props) {
         super(props);
+
+        this.state = {
+            snake: {
+              head: {
+                x: 15,
+                y: 15
+              },
+              tail: {
+                x: 15,
+                y: 15
+              },
+              direction: '',
+              body: [{ x:15, y:15 }],
+              running: false,
+              alive: true,
+              speed: 1
+            }
+        }
     };
 
     drawGrid () {
@@ -37,10 +55,46 @@ class Board extends React.Component {
             ctx: canvas.getContext('2d')
         }, function () {
             this.drawGrid();
+            this.drawSnake();
         });
     };
 
-    componentDidMount() {
+    drawSnake () {
+        const { ctx, snake } = this.state;
+        // Change colour of canvas
+        ctx.fillStyle = 'green';
+        // Loop through the array of the snake body and draw a green box
+        snake.body.forEach (cord => {
+            //ctx.fillRect (cord.x * cellSize, cord.y * cellSize, 1 * cellSize, 1 * cellSize);
+            this.canvasMoveSnake();
+        });
+    };
+
+    // Managing state and canvas
+    changeDirection (direction) {
+        let newState = Object.assign ({}, this.state);
+        newState.snake.direction = direction;
+        this.setState(newState);
+        this.canvasMoveSnake();
+    };
+
+    canvasMoveSnake () {
+        const { ctx, snake } = this.state;
+        // Make the snake's previous position black so it looks like it disappears
+        ctx.fillStyle = 'black';
+        this.drawRect(snake.tail.x,snake.tail.y,1,1);
+        ctx.fillStyle = 'green';
+        this.drawRect(snake.head.x,snake.head.y,1,1);
+        snake.tail.x = snake.head.x;
+        snake.tail.y = snake.head.y;
+    }
+
+    drawRect (x, y, l, h) {
+        const {ctx} = this.state
+        ctx.fillRect(x * cellSize, y * cellSize, l * cellSize, h * cellSize);
+    }
+
+    componentDidMount () {
         this.drawBoard();
     }
 
@@ -48,6 +102,8 @@ class Board extends React.Component {
         return (
             <div id = 'gameContainer' className = 'container-fluid'>
                 <canvas id='gameBoard' ref="gameBoard" width={boardSize} height={boardSize}/>
+                <Snake snake = {this.state.snake}
+                changeDirection = {this.changeDirection.bind(this)}/>
             </div>
         )
     }
